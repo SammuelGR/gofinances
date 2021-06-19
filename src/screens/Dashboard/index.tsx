4a901dcddfd92091832b0aa927831be1a6da625e
Sammuel Reis <sammugr@hotmail.com>
2021-06-19 13:26:50 -0300

    feat: implements initial dashboard header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  Icon,
+  Photo,
+  User,
+  UserInfo,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/23316518?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Sammuel</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
